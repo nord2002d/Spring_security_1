98b01787f00b1d6bbb9112387be6b8c3ffc15679
HP OMEN <nord2002d@yandex.ru>
2023-09-03 20:42:00 +0300

    Внес изменения в скприпт SQL, удалили Util класс.

--- src/main/resources/ScriptSQL/procedure.sql
+++ src/main/resources/ScriptSQL/procedure.sql
@@ -1,5 +1,18 @@
 
-    INSERT INTO users VALUES (1, 11, 'admin', '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq', 'Sokolov');
-    INSERT INTO users VALUES (2, 12, 'user', '$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC', 'Petrov');
+CREATE TABLE IF NOT EXISTS  users (
+                     id INT NOT NULL AUTO_INCREMENT,
+                     name varchar(30) NOT NULL,
+                     sur_name varchar(30) NOT NULL,
+                     age INT NOT NULL,
+                     password varchar(255) NOT NULL,
+                     PRIMARY KEY(id));
+
+    INSERT INTO users VALUES (1, 'admin', 'Sokolov', 11, '$2y$10$UUDoqynp3SsJuWxK6XMQnugF.zt929JjbdRT4N6yG0QUgcV7LKkVq');
+    INSERT INTO users VALUES (2, 'user','Petrov', 12,'$2y$10$hDZIK07X9DFwEyDXd8u7VuaRSvF/TLffc9VHpGdcJTyMwFYNn7msC');
+
+
+CREATE TABLE IF NOT EXISTS user_role (
+     user_id INT NOT NULL,
+     roles  varchar(255) NOT NULL );
     INSERT INTO user_role VALUES (1, 'ADMIN');
     INSERT INTO user_role VALUES (2, 'USER');
